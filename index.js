a88347340d7f5cdaf7e92907fd9becfe7de243ec
Kunall Banerjee <yeskunall96@gmail.com>
2018-01-01 10:58:32 +0530

    Prepare release `v0.1.0`

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-module.exports = (element, callback, options = {}) => {};
+const select = require('select-dom');
+
+module.exports = (element, callback, options = { childList: true }) => {
+  let el = element;
+  if (typeof el === 'string') {
+    el = select(el);
+  }
+
+  if (!el) {
+    throw new Error(`Target ${element} is not a valid instance of \`Node\``);
+  }
+
+  const observer = new MutationObserver(callback);
+  observer.observe(el, options);
+
+  return observer;
+};
